31af3d4a72a0abe1502585d428b862c51005255d
Öznur <oznur@tuta.io>
2022-06-13 19:51:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const reversedWord = reverseString(word);
+  return word === reversedWord;
+}
+
+function reverseString(word) {
+  return word.split("").reverse().join("");
 }
 
 /* 
   Add your pseudocode here
+  Input: string
+  Output: boolean
+  Steps to solve the problem:
+  1. Reverse the string
+  2. Compare the reversed string to the original string
+  3. Return true if they are the same
+  4. Return false if they are not the same
 */
 
 /*
   Add written explanation of your solution here
+  Implemented the given solution on canvas
 */
 
 // You can run `node index.js` to view these console logs
